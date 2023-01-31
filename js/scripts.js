0da55f06a2781374b51cee5c09928b1aa2d60193
Tiantian <tl482@cornell.edu>
2023-01-31 03:22:07 -0500

    lab1

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,32 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+
+
+// Function to increase image size
+// function enlargeImg(id) {
+//     // Get the img object using its Id
+//     img = document.getElementById(id);
+//     // Set image size to 1.5 times original
+//     img.style.transform = "scale(1.5)";
+//     // Animation effect
+//     img.style.transition = "transform 0.25s ease";
+// }
+// // Function to reset image size
+// function resetImg(id) {
+//     // Get the img object using its Id
+//     img = document.getElementById(id);
+//     // Set image size to original
+//     img.style.transform = "scale(1)";
+//     img.style.transition = "transform 0.25s ease";
+// }
+
+function swap(img) {
+    if (img.className !== "cte img-fluid img-centered") { img.style.transform = "scale(2.5)"; img.style.transition = "transform 0.25s ease"; img.className = "cte img-fluid img-centered"; }
+    else {
+        img.style.transform = "scale(1)";
+        img.style.transition = "transform 0.25s ease";
+        img.className = "img-fluid img-centered"
+    }
+}
